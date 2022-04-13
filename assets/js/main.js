5e08622eb1c0dfa9f5c1482f314c9c5258be5cc3
JaneFromAux <post.fuer.j.schaefer@gmail.com>
2022-04-13 16:08:34 +0200

    durch

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+
+
+document.write('<h1>Hello World!</h1>');
+document.write('<p>Have a nice day!</p>');
+
+let myText = 'hello again';
+document.write(myText);
+let name = 'Jane';
+let nachName = 'Wayne';
+document.write(name);
+document.write(nachName);
